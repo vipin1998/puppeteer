a4a903ebc154cd42d4faf097569b2866c1a76e44
vipin1998 <vipindhangar1998@gmail.com>
2018-03-31 01:47:31 +0530

    puppeteer form'

---
+++ creds.js
@@ -0,0 +1,19 @@
+module.exports = {
+    invite_code: 'simplylowrates',
+    email: 'vipindhangar1998@gmail.com',
+    password : 'gravity',
+    first_name : 'Vipin',
+    last_name : 'Vipin',
+    phone : '7690090548',
+    street : 'Ratanada',
+    city : 'Jodhpur',
+    state : 'RI',
+    zip : '342001',
+    ssn : '12345',
+    dob_month : 'Feb',
+    dob_day : '5',
+    dob_year : '1998',
+    income : '40000',
+    mortgage : '12345'
+
+}
